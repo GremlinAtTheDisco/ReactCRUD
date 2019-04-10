14bd94843ed1b76f85a2ce2be6f22f2f62ec12c7
GremlinAtTheDisco <skyesme@gmail.com>
2019-04-10 15:42:46 +0200

    now with loopy addBook

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import BookItem from './BookItem';
 import AddBook from './AddBook';
-import {fetchBooks, requestApiKey} from './api.js';
+//import {fetchBooks, requestApiKey} from './api.js';
 
 const books = [
   {
@@ -16,6 +16,18 @@ const books = [
   }
 ];
 
+const apiKey = localStorage.getItem('firstKey');
+
+const getFirstKey = () => {
+  console.log('blurp', apiKey);
+  if (apiKey) {
+    return apiKey
+  } else {
+    fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?requestKey`)
+      .then(response => response.json())
+      .then(data => localStorage.setItem('firstKey', data.key))
+  }
+};
 
 localStorage.setItem('books', JSON.stringify(books));
 
@@ -24,7 +36,7 @@ class App extends Component {
     super(props)
 
     this.state = {
-      books: JSON.parse(localStorage.getItem('books'))
+      books: JSON.parse(localStorage.getItem('books'), 'title')
     };
     this.onDelete = this.onDelete.bind(this);
     this.onAdd = this.onAdd.bind(this);
@@ -36,8 +48,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const key = fetchBooks();
-    console.log(key);
+
+    getFirstKey();
+    console.log(localStorage.getItem('firstKey'));
   }
 
   getBooks() {
@@ -47,11 +60,21 @@ class App extends Component {
   onAdd(title, author) {
     const books = this.getBooks();
 
-    books.push({
+    /*books.push({
       title,
       author
-    });
-    this.setState({ books });
+    });*/
+
+    fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=${apiKey}&title=${title}&author=${author}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status) {
+          this.onAdd(title, author);
+          this.setState({ books });
+        } else {
+          console.log('something went poopy');
+        }
+      })
   }
 
   onDelete(title) {
